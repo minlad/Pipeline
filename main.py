b2b56fdeee12ac5b075b05b809d23beb83b11fcf
Minesh <mineshl@hotmail.com>
2025-05-26 17:50:27 +0100

    latest update forgot tp push

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import sys
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
+    print('Running')
     print(f'Python Version: {sys.version}')
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 

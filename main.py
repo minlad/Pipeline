3af9fba694df86e019ced3b138a40f10064bcc63
Minesh <mineshl@hotmail.com>
2025-06-05 20:04:36 +0100

    added a new class for the pipeline

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 import sys
+from asyncio import new_event_loop
 
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -18,3 +19,16 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class Person:
+
+    def __init__(self, first_name:str, last_name:str, age:int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def get_full_name(self):
+        return ' '.join([self.first_name, self.last_name])
+
+    def change_last_name(self, new_lastname:str):
+        self.last_name = new_lastname
